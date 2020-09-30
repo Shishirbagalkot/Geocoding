8b7a3bbb8c92007a44422c491d236294db30717e
Shishir Bagalkot <59408046+Shishirbagalkot@users.noreply.github.com>
2020-09-30 11:47:51 +0530

    Volcano web map

---
+++ Geocoding/Map.py
@@ -0,0 +1,31 @@
+import folium
+import pandas
+
+data = pandas.read_csv("volcanoes.txt")
+lat = list(data["LAT"])
+lon = list(data["LON"])
+elev = list(data["ELEV"])
+
+def color_producer(elevation):
+    if elevation < 1000:
+        return 'green'
+    elif 1000 <= elevation < 2500:
+        return 'orange'
+    else:
+        return 'red'    
+
+map = folium.Map(location=[30.56,-90.23], zoom_start=6, tiles = "Stamen Terrain")
+
+fg = folium.FeatureGroup(name="My Map")
+
+for lt, ln, el in zip(lat, lon, elev):
+    fg.add_child(folium.CircleMarker(location = [lt, ln], radius = 6, popup = str(el)+" metres", 
+    fill_color=color_producer(el), color = 'grey', fill_opacity=0.7))
+
+fg.add_child(folium.GeoJson(data=open('world.json', 'r', encoding='utf-8-sig').read(), 
+style_function= lambda x: {'fillColor':'green' if x['properties']['POP2005'] < 10000000
+else 'orange' if 10000000 <= x['properties']['POP2005'] < 20000000 else 'red'}))
+
+map.add_child(fg)
+
+map.save("Test_Map1.html")   
